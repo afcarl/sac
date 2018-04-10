7abb94e17a90c7e2ab30436dff2767f531b7654c
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-04-09 22:46:24 -0700

    Add missing mujoco envs to mujoco_all_sac example

--- examples/mujoco_all_sac.py
+++ examples/mujoco_all_sac.py
@@ -43,6 +43,15 @@ ENVIRONMENTS = {
         'default': HumanoidEnv,
         'multi-direction': MultiDirectionHumanoidEnv,
     },
+    'hopper': {
+        'default': lambda: normalize(GymEnv('Hopper-v1'))
+    },
+    'half-cheetah': {
+        'default': lambda: normalize(GymEnv('HalfCheetah-v1'))
+    },
+    'walker': {
+        'default': lambda: normalize(GymEnv('Walker2d-v1'))
+    },
 }
 
 DEFAULT_DOMAIN = DEFAULT_ENV = 'swimmer'
