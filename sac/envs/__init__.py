6fda85fb4ab3fa0d6f5c2bc6888c1f0bee5c2c05
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-04-09 22:35:25 -0700

    Add missing RandomGoalAntEnv import

--- sac/envs/__init__.py
+++ sac/envs/__init__.py
@@ -4,6 +4,7 @@ from .multi_direction_env import (
     MultiDirectionAntEnv,
     MultiDirectionHumanoidEnv)
 
+from .random_goal_ant_env import RandomGoalAntEnv
 from .cross_maze_ant_env import CrossMazeAntEnv
 from .hierarchy_proxy_env import HierarchyProxyEnv
 from .multigoal import MultiGoalEnv
