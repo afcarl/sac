fixed action prior in policy gradient

sac/algos/sac.py
@@ -273,7 +273,7 @@ class SAC(RLAlgorithm, Serializable):
             self._observations_ph, actions, reuse=True)  # N
 
         if self._reparameterize:
-            policy_kl_loss = tf.reduce_mean(log_pi - log_target + self._vf_t)
+            policy_kl_loss = tf.reduce_mean(log_pi - log_target - policy_prior_log_probs)
         else:
             policy_kl_loss = tf.reduce_mean(log_pi * tf.stop_gradient(
                 log_pi - log_target + self._vf_t - policy_prior_log_probs))